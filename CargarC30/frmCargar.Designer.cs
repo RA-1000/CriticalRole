45010f057997d069e3ffd707e943c1c8be12fda7
ramil <david.garcia@ciber.com>
2016-05-18 09:59:14 +0200

    Primera gran subida.

---
+++ CargarC30/frmCargar.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Conjuros30
+{
+    partial class frmCargar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbEscuela = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNivel = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbComponentes = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbTLanz = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbAlcance = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbAreaEfecto = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbDuracion = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbTSalvacion = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tbResistencia = new System.Windows.Forms.TextBox();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.bGuardar = new System.Windows.Forms.Button();
+            this.bBuscar = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbIDBuscar = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // tbID
+            // 
+            this.tbID.Enabled = false;
+            this.tbID.Location = new System.Drawing.Point(53, 12);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(68, 20);
+            this.tbID.TabIndex = 1;
+            this.tbID.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(232, 12);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(395, 20);
+            this.tbNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Escuela";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nivel";
+            // 
+            // tbEscuela
+            // 
+            this.tbEscuela.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEscuela.Location = new System.Drawing.Point(149, 42);
+            this.tbEscuela.Name = "tbEscuela";
+            this.tbEscuela.Size = new System.Drawing.Size(370, 20);
+            this.tbEscuela.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Componentes";
+            // 
+            // tbNivel
+            // 
+            this.tbNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNivel.Location = new System.Drawing.Point(149, 68);
+            this.tbNivel.Name = "tbNivel";
+            this.tbNivel.Size = new System.Drawing.Size(370, 20);
+            this.tbNivel.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "T. lanz.";
+            // 
+            // tbComponentes
+            // 
+            this.tbComponentes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbComponentes.Location = new System.Drawing.Point(149, 94);
+            this.tbComponentes.Name = "tbComponentes";
+            this.tbComponentes.Size = new System.Drawing.Size(370, 20);
+            this.tbComponentes.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Alcance";
+            // 
+            // tbTLanz
+            // 
+            this.tbTLanz.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTLanz.Location = new System.Drawing.Point(149, 120);
+            this.tbTLanz.Name = "tbTLanz";
+            this.tbTLanz.Size = new System.Drawing.Size(370, 20);
+            this.tbTLanz.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(61, 175);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "A. efecto";
+            // 
+            // tbAlcance
+            // 
+            this.tbAlcance.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAlcance.Location = new System.Drawing.Point(149, 146);
+            this.tbAlcance.Name = "tbAlcance";
+            this.tbAlcance.Size = new System.Drawing.Size(370, 20);
+            this.tbAlcance.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(61, 201);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Duracion";
+            // 
+            // tbAreaEfecto
+            // 
+            this.tbAreaEfecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAreaEfecto.Location = new System.Drawing.Point(149, 172);
+            this.tbAreaEfecto.Name = "tbAreaEfecto";
+            this.tbAreaEfecto.Size = new System.Drawing.Size(370, 20);
+            this.tbAreaEfecto.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 278);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Descripción";
+            // 
+            // tbDuracion
+            // 
+            this.tbDuracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDuracion.Location = new System.Drawing.Point(149, 198);
+            this.tbDuracion.Name = "tbDuracion";
+            this.tbDuracion.Size = new System.Drawing.Size(370, 20);
+            this.tbDuracion.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(61, 227);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(86, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Tir. Salvación";
+            // 
+            // tbTSalvacion
+            // 
+            this.tbTSalvacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTSalvacion.Location = new System.Drawing.Point(149, 224);
+            this.tbTSalvacion.Name = "tbTSalvacion";
+            this.tbTSalvacion.Size = new System.Drawing.Size(370, 20);
+            this.tbTSalvacion.TabIndex = 19;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(61, 253);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(73, 13);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Resistencia";
+            // 
+            // tbResistencia
+            // 
+            this.tbResistencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbResistencia.Location = new System.Drawing.Point(149, 250);
+            this.tbResistencia.Name = "tbResistencia";
+            this.tbResistencia.Size = new System.Drawing.Size(370, 20);
+            this.tbResistencia.TabIndex = 21;
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDescripcion.Location = new System.Drawing.Point(0, 294);
+            this.tbDescripcion.Multiline = true;
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDescripcion.Size = new System.Drawing.Size(705, 272);
+            this.tbDescripcion.TabIndex = 23;
+            // 
+            // bGuardar
+            // 
+            this.bGuardar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bGuardar.Location = new System.Drawing.Point(616, 257);
+            this.bGuardar.Name = "bGuardar";
+            this.bGuardar.Size = new System.Drawing.Size(75, 23);
+            this.bGuardar.TabIndex = 27;
+            this.bGuardar.Text = "Guardar";
+            this.bGuardar.UseVisualStyleBackColor = true;
+            this.bGuardar.Click += new System.EventHandler(this.bGuardar_Click);
+            // 
+            // bBuscar
+            // 
+            this.bBuscar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bBuscar.Location = new System.Drawing.Point(618, 175);
+            this.bBuscar.Name = "bBuscar";
+            this.bBuscar.Size = new System.Drawing.Size(75, 23);
+            this.bBuscar.TabIndex = 26;
+            this.bBuscar.Text = "Buscar";
+            this.bBuscar.UseVisualStyleBackColor = true;
+            this.bBuscar.Click += new System.EventHandler(this.bBuscar_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(599, 152);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(20, 13);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "ID";
+            // 
+            // tbIDBuscar
+            // 
+            this.tbIDBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbIDBuscar.Location = new System.Drawing.Point(625, 149);
+            this.tbIDBuscar.Name = "tbIDBuscar";
+            this.tbIDBuscar.Size = new System.Drawing.Size(68, 20);
+            this.tbIDBuscar.TabIndex = 25;
+            this.tbIDBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnNuevo.Location = new System.Drawing.Point(616, 228);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 26;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // frmCargar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 566);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.bBuscar);
+            this.Controls.Add(this.bGuardar);
+            this.Controls.Add(this.tbDescripcion);
+            this.Controls.Add(this.tbResistencia);
+            this.Controls.Add(this.tbTSalvacion);
+            this.Controls.Add(this.tbDuracion);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.tbAreaEfecto);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbAlcance);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbTLanz);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbComponentes);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbNivel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbEscuela);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbIDBuscar);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "frmCargar";
+            this.Text = "Cargar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbEscuela;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNivel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbComponentes;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbTLanz;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbAlcance;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbAreaEfecto;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbDuracion;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tbTSalvacion;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tbResistencia;
+        private System.Windows.Forms.TextBox tbDescripcion;
+        private System.Windows.Forms.Button bGuardar;
+        private System.Windows.Forms.Button bBuscar;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tbIDBuscar;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
---
+++ CargarC30/frmCargar.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conjuros30
+{
+    public partial class frmCargar : Form
+    {
+        public frmCargar()
+        {
+            InitializeComponent();
+        }
+
+        private void bBuscar_Click(object sender, EventArgs e)
+        {
+            // Instanciamos el contexto y cargamos un listado
+            ConjurosEntities db = new ConjurosEntities();
+
+            long idConjuroBuscar = long.Parse(tbIDBuscar.Text);
+
+            CONJURO conj = (from c in db.CONJURO
+                            where c.ID_CONJURO == idConjuroBuscar
+                            select c).FirstOrDefault();
+
+            // Cargamos el encontrado
+            LimpiarPantalla();
+            CargarConjuro(conj);
+        }
+
+        private void CargarConjuro(CONJURO conj)
+        {
+            if (conj != null)
+            {
+                tbID.Text = conj.ID_CONJURO.ToString();
+                tbNombre.Text = conj.NOMBRE;
+                tbEscuela.Text = conj.ESCUELA;
+                tbNivel.Text = conj.NIVEL;
+                tbComponentes.Text = conj.COMPONENTES;
+                tbTLanz.Text = conj.TIEMPO_LANZAMIENTO;
+                tbAlcance.Text = conj.ALCANCE;
+                tbAreaEfecto.Text = conj.AREA_EFECTO;
+                tbDuracion.Text = conj.DURACION;
+                tbTSalvacion.Text = conj.TIRADA_SALVACION;
+                tbResistencia.Text = conj.RESISTENCIA;
+                tbDescripcion.Text = conj.DESCRIPCION;
+            }
+        }
+
+        private void LimpiarPantalla()
+        {
+            tbID.Text = string.Empty;
+            tbNombre.Text = string.Empty;
+            tbEscuela.Text = string.Empty;
+            tbNivel.Text = string.Empty;
+            tbComponentes.Text = string.Empty;
+            tbTLanz.Text = string.Empty;
+            tbAlcance.Text = string.Empty;
+            tbAreaEfecto.Text = string.Empty;
+            tbDuracion.Text = string.Empty;
+            tbTSalvacion.Text = string.Empty;
+            tbResistencia.Text = string.Empty;
+            tbDescripcion.Text = string.Empty;
+        }
+
+        private void bGuardar_Click(object sender, EventArgs e)
+        {
+            // Instanciamos el contexto y cargamos un listado
+            ConjurosEntities db = new ConjurosEntities();
+
+            // Si el id está vacío, se tratará de una inserción
+            if (String.IsNullOrEmpty(tbID.Text))
+            {
+                // Creamos un nuevo libro y lo añadimos, guardando a continuación los cambios
+                CONJURO c = new CONJURO
+                {
+                    NOMBRE = tbNombre.Text,
+                    ESCUELA = tbEscuela.Text,
+                    NIVEL = tbNivel.Text,
+                    COMPONENTES = tbComponentes.Text,
+                    TIEMPO_LANZAMIENTO = tbTLanz.Text,
+                    ALCANCE = tbAlcance.Text,
+                    AREA_EFECTO = tbAreaEfecto.Text,
+                    DURACION = tbDuracion.Text,
+                    TIRADA_SALVACION = tbTSalvacion.Text,
+                    RESISTENCIA = tbResistencia.Text,
+                    DESCRIPCION = tbDescripcion.Text,
+                };
+                db.CONJURO.Add(c);
+                db.SaveChanges();
+            }
+            else // En caso contrario, se tratará de una modificación
+            {
+                // Recuperamos el conjuro cuyo identificador coincida con el ID
+                long cid = long.Parse(tbID.Text);
+                CONJURO c = db.CONJURO.Where(conj => conj.ID_CONJURO == cid).First();
+
+                // Modificamos el contenido
+                c.NOMBRE = tbNombre.Text;
+                c.ESCUELA = tbEscuela.Text;
+                c.NIVEL = tbNivel.Text;
+                c.COMPONENTES = tbComponentes.Text;
+                c.TIEMPO_LANZAMIENTO = tbTLanz.Text;
+                c.ALCANCE = tbAlcance.Text;
+                c.AREA_EFECTO = tbAreaEfecto.Text;
+                c.DURACION = tbDuracion.Text;
+                c.TIRADA_SALVACION = tbTSalvacion.Text;
+                c.RESISTENCIA = tbResistencia.Text;
+                c.DESCRIPCION = tbDescripcion.Text;
+
+                // Guardamos los cambios
+                db.SaveChanges();
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarPantalla();
+        }
+    }
+}
